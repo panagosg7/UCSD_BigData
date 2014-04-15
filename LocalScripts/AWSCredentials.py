fa2c4097995732e0ed37a1dc7d3b1c14fbbe8189
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-04-14 19:14:56 -0700

    Removing AWSCredentials.py (again...)

--- LocalScripts/AWSCredentials.py
+++
@@ -1,7 +0,0 @@
-### AWS credentials: ####
-aws_access_key_id='AKIAIU3HYRON2IMQGQZQ'
-aws_secret_access_key='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'
-keyPairFile='/home/pvekris/.ssh/UbuntuKey.pem' # name of local file storing keypair
-key_name="UbuntuKey"         # name of keypair on AWS
-security_groups=['PVekrisGroup']
-### End of AWS credentials ####
