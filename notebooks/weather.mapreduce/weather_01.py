40482a6adc1837313e9b6bddd82486b8b379d950
Panagiotis Vekris <pvekris@eng.ucsd.edu>
2014-06-08 01:54:46 -0700

    Two new partitionings.

--- notebooks/weather.mapreduce/weather_01.py
+++ notebooks/weather.mapreduce/weather_01.py
@@ -13,23 +13,41 @@ class MRWeight(MRJob):
     def mapper(self, _, line):
         try:
             self.increment_counter('MrJob Counters','mapper-all',1)
-            elements=line.split(',')
+            elements=line.split(',')             
    
-            # Take all measurements into account!
             if elements[0]!='station':
                 out = (elements[0],1)
             else:
                 out = None
+                
+#                         try:
+#             self.increment_counter('MrJob Counters','mapper-all',1)
+#             elements=line.split(',')
+   
+#             if elements[0]!='station':
+                
+#                 if elements[1] in ['TMIN','TMAX','PRCP']:
+                    
+#                     emp = elements.count('')
+#                     tot = len(elements) - 3
+                    
+#                     # more than 70% full
+#                     if float(emp)/tot < 0.3:
+#                         yield (elements[0],1)
+                    
+                     
+                
                      
         except Exception, e:
             stderr.write('Error in line:\n'+line)
             stderr.write(e)
             self.increment_counter('MrJob Counters','mapper-error',1)
-            out = None # ('error',(1,1))
+            out = None
 
         finally:
             if out != None:
-                yield out
+                pass
+            #yield out
         
     def combiner(self, word, counts):
         self.increment_counter('MrJob Counters','combiner',1)
